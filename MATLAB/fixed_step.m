a7ace521f3ca97919227a491a3e109b19aefee8d
babaraga322 <cleverestbou@gmail.com>
2021-05-08 18:25:44 +0600

    Create fixed_step.m

---
+++ MATLAB/fixed_step.m
@@ -0,0 +1,28 @@
+f = @(x) x^2 - sin(x);
+a = -4;
+b = 6;
+
+
+
+delta = 1e-4;
+
+
+x1 = a;
+x2 = x1 + delta;
+x3 = x2 + delta;
+
+while true
+
+    iter = iter + 1;
+    if f(x1)>=f(x2)
+       if f(x3)>f(x2)
+          break 
+       end
+    end   
+    x1 = x2;
+    x2 = x3;
+    x3 = x2 + delta;
+end
+    
+[x1 x3]
+iter
